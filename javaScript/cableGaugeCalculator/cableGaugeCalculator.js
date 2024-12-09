8276dab6dcc7d4904e40b30fce96a824a34883e5
Joelson Junior <joelson.ejr@gmail.com>
2024-12-09 13:27:08 -0300

    Gauge calculator using TypeScript.

--- javaScript/cableGaugeCalculator/cableGaugeCalculator.js
+++ javaScript/cableGaugeCalculator/cableGaugeCalculator.js
@@ -4,17 +4,37 @@ var VariationCalculator = function (voltage, variation) {
 var AmperageCalculator = function (power, voltage) {
     return power / voltage;
 };
+var wireGauges = [1.5, 2.5, 4.0, 6.0, 10.0, 16.0];
+var convertToStandardGaugeSize = function (size) {
+    var standardSize = 0;
+    for (var i = 0; i < wireGauges.length; i++) {
+        if (size < wireGauges[i]) {
+            standardSize = wireGauges[i];
+            return standardSize;
+        }
+    }
+    return standardSize;
+};
 var gaugeCalculator = function (amperage, distance, conductivity, drop) {
-    var calculateGage = (amperage * (distance * 2)) / (conductivity * drop);
-    var gaugeSize = 0;
+    var calculatedGage = (amperage * (distance * 2)) / (conductivity * drop);
+    var gaugeSize = convertToStandardGaugeSize(calculatedGage);
     return gaugeSize;
 };
+// const readline = require("node:readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 var copperConductivity = 58;
 var variationLimit = 0.04;
 var circuitPower = 5500;
 var circuitVoltage = 127;
 var circuitDistance = 30;
+// rl.question(`Qual a tensão do circuito?`, async (voltage: number) => {
+//   circuitVoltage = voltage;
+//   rl.close();
+// });
 var circuitAmperage = AmperageCalculator(circuitPower, circuitVoltage);
 var voltageDrop = VariationCalculator(circuitVoltage, variationLimit);
 var cableGauge = gaugeCalculator(circuitAmperage, circuitDistance, copperConductivity, voltageDrop);
-console.log("Required cable gauge is ".concat(cableGauge, " mm"));
+console.log("Required cable gauge is ".concat(cableGauge, " mm."));
--- javaScript/cableGaugeCalculator/cableGaugeCalculator.ts
+++ javaScript/cableGaugeCalculator/cableGaugeCalculator.ts
@@ -6,24 +6,51 @@ const AmperageCalculator = (power: number, voltage: number) => {
   return power / voltage;
 };
 
+const wireGauges: number[] = [1.5, 2.5, 4.0, 6.0, 10.0, 16.0];
+
+const convertToStandardGaugeSize = (size: number) => {
+  let standardSize: number = 0;
+
+  for (let i = 0; i < wireGauges.length; i++) {
+    if (size < wireGauges[i]) {
+      standardSize = wireGauges[i];
+      return standardSize;
+    }
+  }
+
+  return standardSize;
+};
+
 const gaugeCalculator = (
   amperage: number,
   distance: number,
   conductivity: number,
   drop: number
 ) => {
-  const calculateGage: number =
+  const calculatedGage: number =
     (amperage * (distance * 2)) / (conductivity * drop);
 
-  const gaugeSize: number = 0;
+  const gaugeSize = convertToStandardGaugeSize(calculatedGage);
+
   return gaugeSize;
 };
+
+// const readline = require("node:readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
 const copperConductivity = 58;
 const variationLimit = 0.04;
 const circuitPower = 5500;
-const circuitVoltage = 127;
+let circuitVoltage = 127;
 const circuitDistance = 30;
+
+// rl.question(`Qual a tensão do circuito?`, async (voltage: number) => {
+//   circuitVoltage = voltage;
+//   rl.close();
+// });
 
 const circuitAmperage = AmperageCalculator(circuitPower, circuitVoltage);
 const voltageDrop = VariationCalculator(circuitVoltage, variationLimit);
